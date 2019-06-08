847c266816cf370795eaafc97ff0d63458927967
Elijah Manor <elijah.manor@gmail.com>
2019-06-08 09:03:23 -0500

    Added docs build so can gh_pages

---
+++ precache-manifest.2bc350e3830472b1b528f9ef763f8601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8b8b3a0b86cb236db611e36fc3058d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2d6a4fd41795f3cc312",
+    "url": "/static/css/2.9f6f48a5.chunk.css"
+  },
+  {
+    "revision": "1614b5e4acf57d9b2a74",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b2d6a4fd41795f3cc312",
+    "url": "/static/js/2.bb6dbeba.chunk.js"
+  },
+  {
+    "revision": "1614b5e4acf57d9b2a74",
+    "url": "/static/js/main.e6ca16be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
