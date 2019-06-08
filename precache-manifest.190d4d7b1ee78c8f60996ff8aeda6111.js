dc5618573b9471fdcdc232767fbffcdcb84838ae
Elijah Manor <elijah.manor@gmail.com>
2019-06-08 09:38:36 -0500

    Try window.location.pathname

--- precache-manifest.190d4d7b1ee78c8f60996ff8aeda6111.js
+++ precache-manifest.190d4d7b1ee78c8f60996ff8aeda6111.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "900282df75c872b77edefab3d4739a41",
+    "revision": "b2fa2b2b6fedb6a2086c0bc82b182bed",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.9f6f48a5.chunk.css"
   },
   {
-    "revision": "1992017de6449c969b48",
+    "revision": "35973b3f8c7aad146f0f",
     "url": "./static/css/main.34de6062.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.bb6dbeba.chunk.js"
   },
   {
-    "revision": "1992017de6449c969b48",
-    "url": "./static/js/main.ba4e6482.chunk.js"
+    "revision": "35973b3f8c7aad146f0f",
+    "url": "./static/js/main.af6468ec.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
