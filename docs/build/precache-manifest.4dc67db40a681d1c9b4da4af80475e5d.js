ed99f9c995576962d20646ba50a1fe28edcea96a
Elijah Manor <elijah.manor@gmail.com>
2019-11-21 22:35:16 -0800

    Build docs and publish

---
+++ docs/build/precache-manifest.4dc67db40a681d1c9b4da4af80475e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f776e1eee441b8674c832fc89aa2855e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7ac4587c7e4b27db1d2a",
+    "url": "./static/css/2.9f6f48a5.chunk.css"
+  },
+  {
+    "revision": "b8829524ccbb36212e27",
+    "url": "./static/css/main.8ef02aa8.chunk.css"
+  },
+  {
+    "revision": "7ac4587c7e4b27db1d2a",
+    "url": "./static/js/2.9a2d3724.chunk.js"
+  },
+  {
+    "revision": "b8829524ccbb36212e27",
+    "url": "./static/js/main.443488f9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
