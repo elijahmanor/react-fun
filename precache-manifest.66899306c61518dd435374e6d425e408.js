3a4a38f3be7b2ee9821d5c929a7b58548ba424b7
Elijah Manor <elijah.manor@gmail.com>
2019-11-22 07:15:47 -0800

    Opps, forgot some files

---
+++ precache-manifest.66899306c61518dd435374e6d425e408.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20d854067060cf0c9177126547e7ad5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7ac4587c7e4b27db1d2a",
+    "url": "./static/css/2.9f6f48a5.chunk.css"
+  },
+  {
+    "revision": "37b32b94e5bab566f8aa",
+    "url": "./static/css/main.8ef02aa8.chunk.css"
+  },
+  {
+    "revision": "7ac4587c7e4b27db1d2a",
+    "url": "./static/js/2.9a2d3724.chunk.js"
+  },
+  {
+    "revision": "37b32b94e5bab566f8aa",
+    "url": "./static/js/main.a0f86df7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
